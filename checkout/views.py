b43829989f9917f3a233282f853fb5afd833069b
Tes3103 <>
2023-12-26 08:35:38 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ORUmrAYkUuNXs27zCT42H979kGSuhp5K74wce9buIZpZja84adLESat3qziXiMk0BJ1tZWeKO6jzGFaChizA9tx0070s99ygL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
